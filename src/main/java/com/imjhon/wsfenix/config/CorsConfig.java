631ed3e9a99f7d312076facc0a3217c4b727100e
dearjhon93 <dear.jhon.93@gmail.com>
2026-08-28 20:13:29 -0500

    Actualizar origen del frontend en CORS a mi-backend-131554770609.us-central1.run.app

--- src/main/java/com/imjhon/wsfenix/config/CorsConfig.java
+++ src/main/java/com/imjhon/wsfenix/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://sga-fenix-project.web.app")
+                .allowedOrigins("http://localhost:4200", "https://mi-backend-131554770609.us-central1.run.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
